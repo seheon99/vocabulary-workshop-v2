97b46497ccdf517913834572854a0ca1722faa82
Seheon Yu <seheon@seoultech.ac.kr>
2024-07-17 19:27:58 +0900

    Improve user identity presentation in navigation components
    - Added Avatar display to the Sidebar and Navbar user dropdowns to enhance visual representation of the user.
    - Replaced static text with dynamic user information, displaying the user's display name and email, making the UI more personalized and informative.
    - Removed unused imports and streamlined the use of icons to declutter the codebase and maintain consistency across components.
    - Enhanced the types by including the User type from Firebase to ensure robust type-checking and clearer developer communication.

--- src/components/features/layout/user-dropdown.tsx
+++ src/components/features/layout/user-dropdown.tsx
@@ -4,14 +4,11 @@ import {
   ArrowRightStartOnRectangleIcon,
   Cog8ToothIcon,
 } from "@heroicons/react/16/solid";
-import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  UserCircleIcon,
-} from "@heroicons/react/20/solid";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 import { getAuth, signOut } from "firebase/auth";
 
 import {
+  Avatar,
   Dropdown,
   DropdownButton,
   DropdownDivider,
@@ -20,11 +17,12 @@ import {
   DropdownMenu,
   NavbarItem,
   SidebarItem,
-  SidebarLabel,
 } from "@/components/base";
 import { LoginButton } from "@/components/features/auth";
 import { app } from "@/firebase";
 import { useCurrentUser } from "@/hooks";
+
+import type { User } from "firebase/auth";
 
 export function NavbarUserDropdown() {
   const { data: user } = useCurrentUser();
@@ -34,8 +32,8 @@ export function NavbarUserDropdown() {
   }
 
   return (
-    <UserDropdown as={NavbarItem} anchor="bottom end">
-      Account
+    <UserDropdown as={NavbarItem} anchor="bottom end" user={user}>
+      {user.email}
       <ChevronDownIcon />
     </UserDropdown>
   );
@@ -49,9 +47,18 @@ export function SidebarUserDropdown() {
   }
 
   return (
-    <UserDropdown as={SidebarItem} anchor="top start">
-      <UserCircleIcon />
-      <SidebarLabel>Account</SidebarLabel>
+    <UserDropdown as={SidebarItem} anchor="top start" user={user}>
+      <span className="flex min-w-0 items-center gap-3">
+        <Avatar src={user.photoURL} className="size-10" square alt="" />
+        <span className="min-w-0">
+          <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
+            {user.displayName}
+          </span>
+          <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
+            {user.email}
+          </span>
+        </span>
+      </span>
       <ChevronUpIcon />
     </UserDropdown>
   );
@@ -64,6 +71,7 @@ function UserDropdown({
 }: {
   as: typeof NavbarItem | typeof SidebarItem;
   anchor: Parameters<typeof DropdownMenu>[0]["anchor"];
+  user: User;
   children: React.ReactNode;
 }) {
   return (
