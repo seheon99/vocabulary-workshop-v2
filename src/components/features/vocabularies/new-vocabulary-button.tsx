b147d930dd95e12afb8ae03df034af3349d170c0
Seheon Yu <49232918+seheon99@users.noreply.github.com>
2024-09-08 14:43:02 +0900

    Update new-vocabulary-button.tsx
    Remove checking user === null

--- src/components/features/vocabularies/new-vocabulary-button.tsx
+++ src/components/features/vocabularies/new-vocabulary-button.tsx
@@ -1,28 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { toast } from "react-toastify";
 
 import { Button } from "@/components/base";
 import {
   NewCategoryDialog,
   NewVocabularyDialog,
 } from "@/components/features/vocabularies";
-import { useCurrentUser } from "@/hooks";
 
 export function NewVocabularyButton() {
-  const { data: user } = useCurrentUser();
-
   const [isOpenVocabulary, setIsOpenVocabulary] = useState(false);
   const [isOpenCategory, setIsOpenCategory] = useState(false);
-
-  const router = useRouter();
-
-  if (user === null) {
-    toast.error("You need to be logged");
-    router.replace("/");
-  }
 
   return (
     <>
