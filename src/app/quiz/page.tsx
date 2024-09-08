c922df3af9de25b6e7d23d36fc8db400098b7e92
Seheon Yu <seheon@seoultech.ac.kr>
2024-09-08 19:18:21 +0900

    Make /quiz page to client component

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,11 +1,25 @@
-import { redirect } from "next/navigation";
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import useSWR from "swr";
 
 import { findRandomVocabulary } from "@/actions";
+import { Text } from "@/components/base";
 
-export default async function QuizPage() {
-  const vocabulary = await findRandomVocabulary();
-  if (!vocabulary) {
-    return redirect("/vocabularies");
-  }
-  return redirect(`/quiz/${vocabulary.id}`);
+export default function QuizPage() {
+  const { data: vocabulary } = useSWR("random-vocabulary", () => findRandomVocabulary());
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (vocabulary === null) {
+      router.replace("/vocabularies");
+    }
+    if (vocabulary) {
+      router.replace(`/quiz/${vocabulary.id}`);
+    }
+  }, [vocabulary]);
+
+  return <Text>Loading</Text>;
 }
